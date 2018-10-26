c2cbafb6afec81b7670cfa18e26bb680edb0b54f
SergeiKrasilnikov <roux-job@mail.ru>
2018-10-27 00:04:02 +0300

    main, создание сервера и контекста

---
+++ src/main/java/org/stepik/webserver/Main.java
@@ -0,0 +1,11 @@
+package org.stepik.webserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class Main {
+    public static void main(String... args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler();
+    }
+}
