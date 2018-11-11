5d5131b02a507914fd83e133f3d14d16047910ff
SergeiKrasilnikov <roux-job@mail.ru>
2018-11-11 22:15:44 +0300

    Первое задание - сервер, возвращающий значение параметра key.

---
+++ src/main/java/org/stepik/webserver/servlets/MirrorServlet.java
@@ -0,0 +1,14 @@
+package org.stepik.webserver.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MirrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println(req.getParameter("key")==null?"":req.getParameter("key"));
+    }
+}
